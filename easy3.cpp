f066b9d1afcf0918c3a361b8e823b2e434feabb1
rockstarchik <112154161+rockstarchik@users.noreply.github.com>
2022-12-26 13:41:18 +0300

    Update easy3.cpp

--- easy3.cpp
+++ easy3.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int itc_sqrt(int num) {
 	int i = 0;
-	if (num < 0)
-		return -1;
-	while (i * i < num)
+	while ( i * i < num)
 		i++;
-	return i;
+	if(i * i == num)
+		return i;
+	return -1;
 }
 int itc_skv(int num) {
 	if (num <= 0)
@@ -22,21 +22,19 @@ int itc_spr(int a, int b) {
 	return a * b;
 }
 int itc_str(int a, int b, int c) {
-	int p, S;
-	p = (a + b + c) / 2;
-	S = 0;
-	if (a <= 0 || b <= 0 || c <= 0)
+	int p;
+	S = 1;
+	if (a + b <= c || a + c <= b || b + c <= a || a <= 0 || b <= 0 || c <= 0)
 		return -1;
-	if ((a + b) > c && (b + c) > a && (a + c) > b) {
-		p = (p * (p - a) * (p - b) * (p - c));
-		S = itc_sqrt(p);
-		return S;
-	}
+	 double p = (a + b + c) / 2;
+	  while (S*S == ((p * (p - a) * (p - b) * (p - c))))
+		  return S;
 	return -1;
 }
+	
 double itc_scir(int radius) {
 	int pi = 3.14;
-	if (radius <= 0)
+	if (radius < 0)
 		return -1;
 	return pi * radius * radius;
 }

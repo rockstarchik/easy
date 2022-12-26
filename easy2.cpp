6b28aa28eaf2c01def8f37ceaa0a931028588697
rockstarchik <112154161+rockstarchik@users.noreply.github.com>
2022-12-26 14:46:52 +0300

    Add files via upload

--- easy2.cpp
+++ easy2.cpp
@@ -1,6 +1,6 @@
 #include "easy.h"
 using namespace std;
- 
+
 bool itc_iseven(int num) {
 	if (num % 2 == 0)
 		return 1;
--- easy3.cpp
+++ easy3.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int itc_sqrt(int num) {
 	int i = 0;
-	while ( i * i < num)
+	while (i * i < num)
 		i++;
-	if(i * i == num)
+	if (i * i == num)
 		return i;
 	return -1;
 }
@@ -16,24 +16,25 @@ int itc_skv(int num) {
 }
 int itc_spr(int a, int b) {
 	if (a <= 0)
-		return -1;           
+		return -1;
 	if (b <= 0)
 		return -1;
 	return a * b;
 }
 int itc_str(int a, int b, int c) {
-	int p;
 	int S = 1;
 	if (a + b <= c || a + c <= b || b + c <= a || a <= 0 || b <= 0 || c <= 0)
 		return -1;
-	 double p = (a + b + c) / 2;
-	  while (S*S == ((p * (p - a) * (p - b) * (p - c))))
-		  return S;
+	double p = (a + b + c) / 2;
+	while (S * S < ((p * (p - a) * (p - b) * (p - c))))
+		S++;
+	if (S * S == ((p * (p - a) * (p - b) * (p - c))))
+		return S;
 	return -1;
 }
-	
+
 double itc_scir(int radius) {
-	int pi = 3.14;
+	double pi = 3.14;
 	if (radius < 0)
 		return -1;
 	return pi * radius * radius;
